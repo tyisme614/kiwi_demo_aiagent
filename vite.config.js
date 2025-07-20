18ec51e83d857bbba15f279f2e319b818362f135
Yuan <yuant614@gmail.com>
2025-07-20 10:42:43 +0800

    Update vite.config.js
    added allowedHost

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    allowedHosts: ['localhost', 'ai.kiwitech.net.cn'],
     host: '0.0.0.0',
     port: 9999
   }
