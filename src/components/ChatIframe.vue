054e54d0798afa3f613106848d11dd6faad5c0b8
Yuan <yuant614@gmail.com>
2025-07-20 10:53:41 +0800

    Update ChatIframe.vue
    show avatar

--- src/components/ChatIframe.vue
+++ src/components/ChatIframe.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="chat-container">
     <iframe
-        src="http://ai.kiwitech.net.cn/chat/share?shared_id=80de8ede650e11f0944782083e95ce54&from=agent&auth=FiZjQ4NTE4NjUwZjExZjA4Y2E5ODIwOD&visible_avatar=1&locale=zh"
+        src="http://ai.kiwitech.net.cn/chat/share?shared_id=80de8ede650e11f0944782083e95ce54&from=agent&auth=FiZjQ4NTE4NjUwZjExZjA4Y2E5ODIwOD&locale=zh"
         style="width: 100%; height: 100%; min-height: 600px"
         frameborder="0"
     >
